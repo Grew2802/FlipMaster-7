60e158e976849f76038e7db582fd724d08cfa175
Shane <flipadmin@flipmaster7.xyz>
2026-02-08 00:54:30 +1100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     plugins: [react()],
-     base: '/Fliptest2/',
-   })
+export default defineConfig({
+  plugins: [react()],
+  base: '/Fliptest2/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+})
