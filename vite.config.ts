beea4dd00b40f9b76a390c955e00a8dd0c2678f5
Grew2802 <flipadmin@flipmaster7.xyz>
2026-02-08 01:59:58 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FlipMaster-7/',
+  base: '/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
